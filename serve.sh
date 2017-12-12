a22b09403c221844a55d025afbd7401f6a90ea04
Marcos Cano <>
2017-12-12 07:36:14 -0600

    first docker service

--- serve.sh
+++ serve.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker run -e "JEKYLL_ENV=production" -it --rm -v "$PWD":/usr/src/app -p "4000:4000" --init starefossen/github-pages /usr/src/app/.scripts/up.sh
+PORT=4000
+
+open "http://localhost:$PORT/home"
+
+docker run -e "JEKYLL_ENV=production" -it --rm -v "$PWD":/usr/src/app -p "$PORT:4000" --init starefossen/github-pages /usr/src/app/.scripts/up.sh
+
