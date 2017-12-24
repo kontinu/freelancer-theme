166fbe4e5c461044439db63e86a04d09a3b48989
Marcos Cano <>
2017-12-24 00:57:32 -0600

    compose over scripts

--- .scripts/up.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-jekyll server -P 4000 -H 0.0.0.0
--- serve.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PORT=4000
-
-open "http://localhost:$PORT/home/"
-
-docker run -e "JEKYLL_ENV=production" -it --rm -v "$PWD":/usr/src/app -p "$PORT:4000" --init starefossen/github-pages /usr/src/app/.scripts/up.sh
-
