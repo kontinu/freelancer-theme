2dbbe45ef8d00b84f13f11d7c6f555ec1054f9aa
Marcos Cano <>
2017-12-24 00:46:14 -0600

    6 services icons

--- serve.sh
+++ serve.sh
@@ -2,7 +2,7 @@
 
 PORT=4000
 
-open "http://localhost:$PORT/home"
+open "http://localhost:$PORT/home/"
 
 docker run -e "JEKYLL_ENV=production" -it --rm -v "$PWD":/usr/src/app -p "$PORT:4000" --init starefossen/github-pages /usr/src/app/.scripts/up.sh
 
