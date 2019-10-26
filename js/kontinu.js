a622c1871240f3e3930808f35d09e895b139a84e
@jmarcos-cano <jmarcos.cano@gmail.com>
2019-10-26 03:00:23 -0600

    @jmarcos-cano - kontinu.js

--- js/kontinu.js
+++ js/kontinu.js
@@ -4,7 +4,6 @@ window.onscroll = function() {
 
 function growShrinkLogo() {
     var Logo = document.getElementById("Logo")
-    console.log("logo shrink")
     if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
         Logo.style.width = '10rem';
     } else {
